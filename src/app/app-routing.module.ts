1160933e89392f27412fc5b82089f52f714a5c3d
Anirudh Singh Rawat <anirudhrawat1@gmail.com>
2023-10-30 16:27:16 -0700

    basic template without style and logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AgGridModule } from 'ag-grid-angular';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    AgGridModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
